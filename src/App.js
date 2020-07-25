8c2808e29ff7fbdca65cb3d7e470c52d28db630b
Syed Rizvi <57242341+SyedA5688@users.noreply.github.com>
2020-07-25 09:50:45 -0500

    Added file structure, component folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HackOrganized</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>HackOrganized</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
