52d2e213e31555915941420c932e99e5bafb5b52
Syed Rizvi <57242341+SyedA5688@users.noreply.github.com>
2020-07-25 10:00:38 -0500

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/layout/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>HackOrganized</h1>
+        <NavBar />
+        <h1 className="ml-5" >HackOrganized</h1>
       </div>
     </BrowserRouter>
   );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
-export default function Navbar() {
+export default function NavBar() {
   return (
-    
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+        </Nav>
+        <Nav className="mr-sm-2">
+          <Nav.Link href="#account">Account</Nav.Link>
+          <Nav.Link href="#signin">SignIn</Nav.Link>
+        </Nav>
+        
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
