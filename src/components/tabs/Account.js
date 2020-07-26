0791154bde39bd5ff6174af8f719d71ac27b6f38
Melika Nassizadeh <melikanass@gmail.com>
2020-07-26 07:03:03 -0500

    commit

--- src/components/tabs/Account.js
+++ src/components/tabs/Account.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card, Row, Col, Form, Button } from 'react-bootstrap';
 
-export default function Account() {
-  return (
+export default class Account extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authState: 'signedIn',
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      nickname: "",
+      birthday: "",
+      gender: "",
+      phone_number: "",
+      website: "",
+      github:"",
+    };
+    
+  }
+
+  handleChange= (event) =>{
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Profile Updated' + this.state.value);
+    event.preventDefault();
+  }
+  
+  render() {
+    return (
     <Container>
-      <h1 >Account Page</h1>
+      
+      <h1>Your Account</h1>
+      <Card class="card">
+
+      </Card>
+      <Card class="card">
+      <Form class="text" onSubmit={this.handleSubmit}>
+        <input type="text" id="assignment" placeholder="Description/About You" onChange={this.handleChange} />         
+        <Button variant="light" value="Submit" id="submit" type="Update">Update </Button>    
+      </Form>
+      </Card>
+    
+      <Card class="card">
+      <Form class="text" onSubmit={this.handleSubmit}>
+        <Form.Control type="text" id="assignment" placeholder="Description/About You" onChange={this.handleChange} />         
+        <Button variant="light" value="Submit" id="submit" type="Update">Update </Button>    
+      </Form>
+      </Card>
+      
     </Container>
-  )
+    );
+  }
 }
