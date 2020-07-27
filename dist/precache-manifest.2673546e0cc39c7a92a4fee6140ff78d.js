c160332edc5949caa06e8af54d6c14b9dc9a165b
Syed Rizvi <57242341+SyedA5688@users.noreply.github.com>
2020-07-27 12:54:52 -0500

    Reran firebase init, npm run build, and firebase deployed. Web app running on firebase hosting now

---
+++ dist/precache-manifest.2673546e0cc39c7a92a4fee6140ff78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a474aa638fca1452c62b825d40974c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56690e91b4f83f02ca01",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "92d5015bf16ba3d317f2",
+    "url": "/static/css/main.8da355a7.chunk.css"
+  },
+  {
+    "revision": "56690e91b4f83f02ca01",
+    "url": "/static/js/2.e522f8e8.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.e522f8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d5015bf16ba3d317f2",
+    "url": "/static/js/main.022be61b.chunk.js"
+  },
+  {
+    "revision": "cd8b3cdcc6c4e28ae2c7",
+    "url": "/static/js/runtime-main.1658e5ec.js"
+  }
+]);
